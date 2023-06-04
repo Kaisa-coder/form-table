ea35e02beaf8c11de13512bd921f12858c1f19c9
zpf <2384085701@qq.com>
2023-06-04 19:47:43 +0800

    feat/新增test文件

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,10 @@
 <template>
   <!-- <FilterConditions :config="inputConfig" :buttonConfig="buttonConfig" /> -->
-  <TableDate :config="tableConfig" />
 </template>
 
 <script setup>
-import TableDate from "@/packages/tableData/index.vue"
 import FilterConditions from "@/packages/FilterConditions/index.vue"
 import { ref } from "vue";
-// 表格配置
-const tableConfig = ref({
-  thead: [
-    {
-      label: "Date",
-      props: "date"
-    },
-    {
-      label: "Name",
-      props: "name"
-    },
-    {
-      label: "Address",
-      props: "address"
-    },
-    {
-      label: "操作"
-    },
-  ],
-  url: '/tableInfo',
-  checkbox: true
-})
 
 // 筛选条件配置
 const inputConfig = ref({
---
+++ src/test/Table.vue
@@ -0,0 +1,35 @@
+<template>
+  <TableDate :config="tableConfig" />
+</template>
+
+<script setup>
+import TableDate from "@/packages/tableData/index.vue"
+import { ref } from "vue";
+
+// 表格配置
+
+const tableConfig = ref({
+  thead: [
+    {
+      label: "Date",
+      props: "date"
+    },
+    {
+      label: "Name",
+      props: "name"
+    },
+    {
+      label: "Address",
+      props: "address"
+    },
+    {
+      label: "操作"
+    },
+  ],
+  url: '/tableInfo',
+  checkbox: true
+})
+
+
+</script>
+<style lang="scss"></style>
